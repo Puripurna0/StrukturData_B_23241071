8bb3570bbb2c0c166d1e7bc70e2e322e55a8fcac
Puripurna0 <146051287+Puripurna0@users.noreply.github.com>
2024-04-01 12:08:58 +0800

    pertemuan kedua

---
+++ meet2.py
@@ -0,0 +1,20 @@
+# belajar Variable
+# assigment
+x = 3
+y = 6
+uang = 1000
+
+#pemanggilan
+print ("Nilai UAS saya =", x)
+print ("Nilai UTS saya =", y)
+print ("sisa uang saya =", uang)
+
+# penamaan variable
+nilai_a = 9 # tidak boleh spasi
+kontak10 = 10 # tidak boleh diawali angka
+
+# input variable kedua
+x = 3000
+print ("Nilai UAS saya =", x)
+x = 2500
+print ("Nilai UTS saya =", x)
