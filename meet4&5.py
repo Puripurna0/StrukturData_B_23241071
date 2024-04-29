c26f30450358b2bc022d9638f270b8600577a851
Puripurna0 <146051287+Puripurna0@users.noreply.github.com>
2024-04-29 11:51:24 +0800

    senin, 29/april/2024

---
+++ meet4&5.py
@@ -0,0 +1,48 @@
+# inputan dari user
+# aritmatika
+
+# Belajar Inputan
+#Lisa = input("masukkan kata : ")
+#print("isi dari lisa :", Lisa,"bertipe data :", type(Lisa))
+
+# Belajar aritmatika dasar
+x = 2
+y = 7
+
+# penjumlahan +
+hasil = x + y
+print ("x + y =",hasil )
+
+# pengurangan -
+hasil = x - y
+print ("x - y =",hasil )
+
+# perkalian *
+hasil = x * y
+print ("x * y =", hasil )
+
+# pembagian /
+hasil = x / y
+print ("x / y =", hasil )
+
+# perpangkatan exponen **
+hasil = x ** y
+print ("x ^ y =", hasil )
+
+# modulus %
+hasil = y % x
+print ("x mod y =",hasil )
+
+# floor division (pembulatan kebawah) //
+hasil = x // y
+print ("x // y =",hasil )
+
+# aritmatika prioritas
+# (), exponen, perkalian/pembagian, penjumlahan/pengurangan
+a = 4
+b = 2
+c = 7
+
+hasil = ( a ** b * (c + a) / b - c % a // b)
+print(hasil)
+
