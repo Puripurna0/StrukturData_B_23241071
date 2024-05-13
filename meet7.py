b64272a16800ec156bcf43fb30b860f7966b1dcd
Puripurna0 <146051287+Puripurna0@users.noreply.github.com>
2024-05-13 11:59:53 +0800

    senin, 13/05/2024

---
+++ meet7.py
@@ -0,0 +1,68 @@
+# Tabel kebenaran (logika bolean)
+# not and or xor
+
+# NOT
+print("*****Logika NOT*****")
+X = False
+Y = not X
+print('value x =', X)
+print('***********NOT')
+print('value y =', Y)
+
+#OR
+print("*****Logika OR*****")
+X = False
+Y = False
+Z = x or y
+print(x, 'OR', y, '=', z)
+X = True
+Y = False
+z = x or y
+print(x, 'OR', y, '=', z)
+x = True
+Y = False
+z = x or y
+print(x, 'OR', y, '=', z)
+x = True
+Y = False
+z = x or y 
+
+# AND 
+print("*****Logika AND*****")
+X = False
+Y = False
+Z = x or y
+print(x, 'AND', y, '=', z)
+X = True
+Y = False
+z = x or y
+print(x, 'AND', y, '=', z)
+x = True
+Y = False
+z = x or y
+print(x, 'AND', y, '=', z)
+x = True
+Y = False
+z = x or y 
+
+# XOR
+print("*****Logika XOR*****")
+X = False
+Y = False
+Z = x ^ y
+print(x, 'XOR', y, '=', z)
+X = True
+Y = False
+z = x ^ y
+print(x, 'XOR', y, '=', z)
+x = True
+Y = False
+z = x ^ y
+print(x, 'XOR', y, '=', z)
+x = True
+Y = False
+z = x ^ y 
+
+
+
+
